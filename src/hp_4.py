4438906ea42ae04d10f25dab8b21b5c020d47916
ranjithkumarkari <rkari1@cbu.edu>
2024-04-26 03:15:30 -0500

    HW4

--- src/hp_4.py
+++ src/hp_4.py
@@ -6,32 +6,21 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    reformatted_dates = []
-    for date_str in old_dates:
-        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
-        reformatted_date = date_obj.strftime('%d %b %Y')
-        reformatted_dates.append(reformatted_date)
-    return reformatted_dates
+    pass
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    if not isinstance(start, str):
-        raise TypeError("Start date should be a string.")
-    if not isinstance(n, int):
-        raise TypeError("Number of days should be an integer.")
-    start_date = datetime.strptime(start, '%Y-%m-%d')
-    return [start_date + timedelta(days=i) for i in range(n)]
+    pass
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    dates = date_range(start_date, len(values))
-    return list(zip(dates, values))
+    pass
 
 
 def fees_report(infile, outfile):
@@ -42,7 +31,7 @@ def fees_report(infile, outfile):
         reader = DictReader(file)
         for row in reader:
             due_date = datetime.strptime(row['date_due'], '%m/%d/%Y')
-            return_date = datetime.strptime(row['date_returned'], '%m/%d/%y')
+            return_date = datetime.strptime(row['date_returned'], '%m/%d/%Y')
             if return_date > due_date:
                 days_late = (return_date - due_date).days
                 late_fee = days_late * 0.25
@@ -54,3 +43,29 @@ def fees_report(infile, outfile):
         writer.writeheader()
         for patron_id, fee in late_fees.items():
             writer.writerow({'patron_id': patron_id, 'late_fees': '{:.2f}'.format(fee)})
+
+
+# The following main selection block will only run when you choose
+# "Run -> Module" in IDLE.  Use this section to run test code.  The
+# template code below tests the fees_report function.
+#
+# Use the get_data_file_path function to get the full path of any file
+# under the data directory.
+
+if __name__ == '__main__':
+
+    try:
+        from src.util import get_data_file_path
+    except ImportError:
+        from util import get_data_file_path
+
+    # BOOK_RETURNS_PATH = get_data_file_path('book_returns.csv')
+    BOOK_RETURNS_PATH = get_data_file_path('book_returns_short.csv')
+
+    OUTFILE = 'book_fees.csv'
+
+    fees_report(BOOK_RETURNS_PATH, OUTFILE)
+
+    # Print the data written to the outfile
+    with open(OUTFILE) as f:
+        print(f.read())
